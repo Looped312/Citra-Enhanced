cbd43f0c090169a632d09059fad399bfc369d547
Looped312 <139386975+Looped312@users.noreply.github.com>
2024-05-12 23:21:43 -0400

    Create EditOverlayViewModel.kt
    Created and found useful imports for creating a dedicated overlay editor

---
+++ src/android/app/src/main/java/org/citra/citra_emu/viewmodel/EditOverlayViewModel.kt
@@ -0,0 +1,13 @@
+package org.citra.citra_emu.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import org.citra.citra_emu.overlay.*
+import android.content.Context
+import org.citra.citra_emu.fragments.EmulationFragment
+//probably use emuation fragment because that's where the overlay is used
+
+
+class EditOverlayViewModel : ViewModel() {
+
+}
